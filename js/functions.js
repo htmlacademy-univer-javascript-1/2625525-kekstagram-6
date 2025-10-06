68d6f0b7d9e76f79d988c46048b7d4141257f77b
Илья Константинов <ilak29866@gmail.com>
2025-10-06 16:50:02 +0500

    Исправления файла function

--- js/functions.js
+++ js/functions.js
@@ -23,27 +23,3 @@ function extractNumbers(strOrNum) {
   }
   return numberStr ? parseInt(numberStr, 10) : NaN;
 }
-
-
-
-/Тесты на длину строки/
-(lengthElement('проверяемая строка', 20));
-(lengthElement('проверяемая строка', 18));
-(lengthElement('проверяемая строка', 10));
-
-/Тесты на палиндром/
-(palindrome('топот'));
-(palindrome('ДовОд'));
-(palindrome('ШАЛАШ'));
-(palindrome('Кекс'));
-(palindrome('Лёша на полке клопа нашёл '));
-
-/Тесты Извлечения числа из строки/
-(stringNumber('2023 год'));
-(stringNumber('ECMAScript 2022'));
-(stringNumber('1 кефир, 0.5 батона'));
-(stringNumber('агент 007'));
-(stringNumber('а я томат'));
-(stringNumber(-1));
-(stringNumber(1.5));
-(stringNumber(-12.5));
